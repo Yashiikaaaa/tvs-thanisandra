31df2b8c155b3d17049c37b0b3915dbc10bf23d7
yashiikaaaa <yashika@truestate.in>
2025-07-20 11:49:15 +0530

    spelling

--- src/Pages/PlotDetails.tsx
+++ src/Pages/PlotDetails.tsx
@@ -27,7 +27,7 @@ export default function PlotDetailsPage({ openModal }: PlotDetailsProp) {
     <div id="floorplan" className="bg-[#f2fbe7] w-full py-10 px-10 flex flex-col items-center md:items-start z-10 scroll-mt-16">
       {/* Section Heading */}
       <h2 className="text-[#26650B] font-semibold text-base md:text-xl border-3 border-[#26650B] px-8 py-1 mx-10 rounded-full mb-6 whitespace-nowrap">
-        Congigurations
+        Configurations
       </h2>
 
       {/* Layout Container */}
